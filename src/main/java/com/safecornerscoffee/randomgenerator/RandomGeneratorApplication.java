2198cf2d30cce7e0232382c3d2620a4d3b93f6ae
safecorners <safeconers.mobile@gmail.com>
2022-04-24 07:56:17 +0900

    create spring-boot sample app

---
+++ src/main/java/com/safecornerscoffee/randomgenerator/RandomGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.randomgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGeneratorApplication.class, args);
+	}
+
+}
